b3901016d884c8c1991168404e733db05bd30774
DinaDinaa <133159061+DinaDinaa@users.noreply.github.com>
2024-09-23 18:38:41 +0400

    Add files via upload

---
+++ 1 task.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+Console.WriteLine("Write your number");
+var usernumber = int.Parse(Console.ReadLine()); 
+if (usernumber < 0)
+{
+    Console.WriteLine("Your number is negative");
+}
+else if (usernumber > 0)
+{
+    Console.WriteLine("Your number is positive");
+}
+else if (usernumber == 0)
+{
+    Console.WriteLine("Your number is zero");
+}
