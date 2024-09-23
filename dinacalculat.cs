598185c858e5e275b936d6ce32edcdfe821fddb0
DinaDinaa <133159061+DinaDinaa@users.noreply.github.com>
2024-09-23 13:14:25 +0400

    Add files via upload

---
+++ dinacalculat.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("number1");
+var a = float.Parse(Console.ReadLine());
+Console.WriteLine("number2");
+var b = float.Parse(Console.ReadLine());
+Console.WriteLine("choose operation");
+var moqmedeba = Console.ReadLine();
+
+//Console.WriteLine($"{a} + {b} = {a + b}")
+
+if (moqmedeba == "+")
+{
+    Console.WriteLine( a+b );
+
+}
+else if (moqmedeba == "-")
+{
+    Console.WriteLine(a - b);
+}
+else if (moqmedeba == "*")
+{
+    Console.WriteLine(a * b);
+}
+else if (moqmedeba == "/" && b != 0)
+{
+    Console.WriteLine(a / b);
+}
+else if (moqmedeba == "/" && b == 0)
+{
+    Console.WriteLine("error");
+}
+
