6823741aa100e4f7893eb1fea57a3f5cb9ddc2a7
DinaDinaa <133159061+DinaDinaa@users.noreply.github.com>
2024-09-25 18:35:32 +0400

    7

--- dinacalculat.cs
+++ dinacalculat.cs
@@ -5,28 +5,28 @@ var a = float.Parse(Console.ReadLine());
 Console.WriteLine("number2");
 var b = float.Parse(Console.ReadLine());
 Console.WriteLine("choose operation");
-var moqmedeba = Console.ReadLine();
+var operation = Console.ReadLine();
 
 //Console.WriteLine($"{a} + {b} = {a + b}")
 
-if (moqmedeba == "+")
+if (operation == "+")
 {
     Console.WriteLine( a+b );
 
 }
-else if (moqmedeba == "-")
+else if (operation == "-")
 {
     Console.WriteLine(a - b);
 }
-else if (moqmedeba == "*")
+else if (operation == "*")
 {
     Console.WriteLine(a * b);
 }
-else if (moqmedeba == "/" && b != 0)
+else if (operation == "/" && b != 0)
 {
     Console.WriteLine(a / b);
 }
-else if (moqmedeba == "/" && b == 0)
+else if (operation == "/" && b == 0)
 {
     Console.WriteLine("error");
 }
