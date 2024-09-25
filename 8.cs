7f7c8509689b6a5a44a6df93802c3009c3985cd7
DinaDinaa <133159061+DinaDinaa@users.noreply.github.com>
2024-09-25 19:30:23 +0400

    Add files via upload

---
+++ 8.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("How old are you?");
+var userinputyears = int.Parse(Console.ReadLine());
+if (userinputyears >= 18)
+{
+    Console.WriteLine("Available");
+}
+else
+{
+    Console.WriteLine("Not available");
+}
+
