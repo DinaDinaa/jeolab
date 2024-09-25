20c3dfa9355becd13b0e5e92ba4b8d7199fa009a
DinaDinaa <133159061+DinaDinaa@users.noreply.github.com>
2024-09-25 18:49:04 +0400

    Add files via upload

---
+++ 5.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter your password");
+var userinput = (Console.ReadLine());
+var rightpassword = ("secret123");
+if (userinput == rightpassword)
+{
+    Console.WriteLine("Access Granted");
+}
+else
+{
+    Console.WriteLine("Incorrect password");
+}
