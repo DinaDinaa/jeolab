68477502512db86937bde2ba03d9237ef11def2a
DinaDinaa <133159061+DinaDinaa@users.noreply.github.com>
2024-09-25 17:40:36 +0400

    Add files via upload

---
+++ 2.cs
@@ -0,0 +1,14 @@
+﻿var usernumber = int.Parse(Console.ReadLine());
+
+if (usernumber == 0)
+{
+    Console.WriteLine("Your number is 0");
+}
+else if (usernumber % 2 == 0)
+{
+    Console.WriteLine("Your number is even");
+}
+else if (usernumber % 2 == 1)
+{
+    Console.WriteLine("Your number is odd");
+}
