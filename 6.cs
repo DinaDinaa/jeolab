f17e53bea490806f9a719179ea6c3cbf51b39a69
DinaDinaa <133159061+DinaDinaa@users.noreply.github.com>
2024-09-25 19:23:34 +0400

    Add files via upload

---
+++ 6.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Write your number");
+var number1 = double.Parse(Console.ReadLine());
+var number2 = double.Parse(Console.ReadLine());
+var number3 = double.Parse(Console.ReadLine());
+if (number1 > number2 && number2 > number3)
+{
+    Console.WriteLine($"{number1} is max");
+}
+else if (number2 > number1 && number1 > number3)
+{
+    Console.WriteLine($"{number2} is max");
+}
+else if (number3 > number2 && number2 > number1)
+{
+    Console.WriteLine($"{number3} is max");
+}
+
+
+
