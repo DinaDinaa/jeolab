d56d0fb3cbf9dc772f660b4d5bde0709b6af2ba8
DinaDinaa <133159061+DinaDinaa@users.noreply.github.com>
2024-09-25 17:51:41 +0400

    Add files via upload

---
+++ 4.cs
@@ -0,0 +1,12 @@
+﻿
+var year = int.Parse(Console.ReadLine());
+
+if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)
+{
+    Console.WriteLine("Leap year");
+}
+else
+{
+    Console.WriteLine("Year is not leap");
+}
+
