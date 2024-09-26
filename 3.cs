a09355fe10e3a984f0d4045b35f82caf7baeca38
DinaDinaa <133159061+DinaDinaa@users.noreply.github.com>
2024-09-26 16:22:12 +0400

    Add files via upload

---
+++ 3.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Write your score:");
+var userscore = int.Parse(Console.ReadLine());
+if (userscore >= 90 && userscore <= 100)
+{
+    Console.WriteLine("A");
+}
+else if (userscore >= 80 && userscore < 90)
+{
+    Console.WriteLine("B");
+}
+else if (userscore >= 70 & userscore < 80)
+{
+    Console.WriteLine("C");
+}
+else if (userscore >= 60 && userscore < 70)
+{
+    Console.WriteLine("D");
+}
+else if (userscore >= 0 && userscore < 60)
+{
+    Console.WriteLine("F");
+}
+
+
+
