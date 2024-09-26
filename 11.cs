c478fb4f80f9e443c10963bb806768cd26f1fd12
DinaDinaa <133159061+DinaDinaa@users.noreply.github.com>
2024-09-26 18:00:13 +0400

    Update 11.cs

--- 11.cs
+++ 11.cs
@@ -3,11 +3,11 @@ var country = Console.ReadLine();
 Console.WriteLine("How old are you?");
 var age = int.Parse(Console.ReadLine());
 var country1 = ("USA");
-if (country == country1 && age >= 21)
+if ( country == country1 && age >= 21 )
 {
     Console.WriteLine("Access");
 }
-else if (age >= 18)
+else if ( age >= 18 )
 {
     Console.WriteLine("Access");
 }
