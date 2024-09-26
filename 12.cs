a3ff27661b2d3c1cb40567c7ff0fd4bc21bfd2d4
DinaDinaa <133159061+DinaDinaa@users.noreply.github.com>
2024-09-26 18:16:31 +0400

    Add files via upload

---
+++ 12.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("How old are you?");
+var age = int.Parse(Console.ReadLine());
+var answer = ("yes");
+if (age >= 18)
+{
+    Console.WriteLine("Your salary per year:");
+    var salary = int.Parse(Console.ReadLine());
+    
+    if (salary >= 25000 && age >= 18)
+    {
+        Console.WriteLine("Congratulations!");
+    }
+    else if (salary < 25000 && age >= 18)
+    {
+        Console.WriteLine("Do you have co-borrower?");
+        var useranswer = Console.ReadLine();
+        if (useranswer == answer)
+        {
+            Console.WriteLine("Congratulations!");
+        }
+        else
+        {
+            Console.WriteLine("Without access");
+
+        }
+    }
+    else
+    {
+        Console.WriteLine("Without access");
+    }
+    }
+    else
+    {
+        Console.WriteLine("Without access");
+    }
+
+
+
+
+
+
+
