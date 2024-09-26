91e6557f964949a87a27797342baa6fcbe5d79a6
DinaDinaa <133159061+DinaDinaa@users.noreply.github.com>
2024-09-26 17:40:43 +0400

    Add files via upload

---
+++ 10.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Write amount:");
+var amount = float.Parse(Console.ReadLine());
+Console.WriteLine($" amount is {amount} $");
+var discount1 = amount - 0.1 * amount;
+var discount2 = amount - 0.2 * amount;
+if ( amount < 100 )
+{
+    Console.WriteLine("Without discount");
+}
+else if ( amount >= 100 & amount <= 500 )
+{
+    Console.WriteLine("Discount 10%");
+    Console.WriteLine($"You have to pay {discount1} $");
+}
+else if (amount > 500 )
+{
+    Console.WriteLine("Discount 20%");
+    Console.WriteLine($"You have to pay {discount2} $");
+}
+
+
+
+
