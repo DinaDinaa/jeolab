66866b55970c4bd1b53cb72ca8fe393236f6aa01
DinaDinaa <133159061+DinaDinaa@users.noreply.github.com>
2024-09-26 17:20:20 +0400

    Add files via upload

---
+++ 9.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Hours:");
+var hours = int.Parse(Console.ReadLine());
+Console.WriteLine("Minutes:");
+var minutes = int.Parse(Console.ReadLine());
+
+if (hours >= 0 && hours <= 23 && minutes >= 0 && minutes <= 59)
+{
+    Console.Write($"Time is: {hours}  : {minutes} ");
+}
+else
+{
+    Console.WriteLine("Error");
+}
+if (hours >= 5 && hours <= 11 && minutes >= 0 && minutes <= 59 )
+{
+    Console.WriteLine(" Good morning ");
+}
+else if (hours >= 12 && hours <= 16 && minutes >= 0 && minutes <= 59)
+{
+    Console.WriteLine(" Good afternoon ");
+}
+else if (hours >= 17 && hours <= 20 && minutes >= 0 && minutes <= 59)
+{
+    Console.WriteLine(" Good evening ");
+}
+else if (hours >= 21 && hours <= 4 && minutes >= 0 && minutes <= 59)
+{
+    Console.WriteLine(" Good night ");
+}
+
+
+
