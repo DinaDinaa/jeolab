ce9ac8ce58f47073aea43812f07d64ba7c62f47e
DinaDinaa <133159061+DinaDinaa@users.noreply.github.com>
2024-09-26 17:53:37 +0400

    Add files via upload

---
+++ 11.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Write your country:");
+var country = Console.ReadLine();
+Console.WriteLine("How old are you?");
+var age = int.Parse(Console.ReadLine());
+var country1 = ("USA");
+if (country == country1 && age >= 21)
+{
+    Console.WriteLine("Access");
+}
+else if (age >= 18)
+{
+    Console.WriteLine("Access");
+}
+else
+{
+    Console.WriteLine("Without access");
+}
+
+
+
+
+
